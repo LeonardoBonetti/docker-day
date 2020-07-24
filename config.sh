2edee1729d36babd4a9e1eca2e188d455ece860e
Leonardo Bonetti <leonardo.bonetti.silva@gmail.com>
2020-07-24 00:24:13 -0300

    atualização do config

--- config.sh
+++ config.sh
@@ -9,14 +9,14 @@ export DOLLAR='$'
 PROJECT_ROOT=$(dirname "$0")
 
 mkdir -p $PROJECT_ROOT/front/nginx-conf
-mkdir -p $PROJECT_ROOT/back/nginx-conf
+mkdir -p $PROJECT_ROOT/backend/nginx-conf
 
-envsubst < $PROJECT_ROOT/etc/nginx-back.template > $PROJECT_ROOT/back/nginx-conf/nginx.conf
-envsubst < $PROJECT_ROOT/etc/ecosystem.config.template > $PROJECT_ROOT/back/ecosystem.config.js
+envsubst < $PROJECT_ROOT/etc/nginx-back.template > $PROJECT_ROOT/backend/nginx-conf/nginx.conf
+envsubst < $PROJECT_ROOT/etc/ecosystem.config.template > $PROJECT_ROOT/backend/ecosystem.config.js
 
 envsubst < $PROJECT_ROOT/etc/nginx-front.template > $PROJECT_ROOT/front/nginx-conf/nginx.conf
 
-envsubst < $PROJECT_ROOT/etc/dockerfile-back.template > $PROJECT_ROOT/back/dockerfile
+envsubst < $PROJECT_ROOT/etc/dockerfile-back.template > $PROJECT_ROOT/backend/dockerfile
 envsubst < $PROJECT_ROOT/etc/dockerfile-front.template > $PROJECT_ROOT/front/dockerfile
 
 envsubst < $PROJECT_ROOT/etc/docker-compose.template > $PROJECT_ROOT/docker-compose.yml
